326e488213ee28972239e60884895cb8d82ee7f6
uyplayer <32848979@qq.com>
2020-01-03 19:35:57 +0800

    okk

---
+++ Beginning Linux Programming/Chapter 9/main.c
@@ -0,0 +1,16 @@
+#include <stdlib.h>
+#include "a.h"
+
+extern void function_two(/* arguments */);
+extern void function_three(/* arguments */);
+
+int main(int argc, char const *argv[]) {
+
+      function_two();
+      function_three();
+
+      return 0;
+
+
+
+}
